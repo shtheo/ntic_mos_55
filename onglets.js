f201847fe644b7ce9316d4d5ecc04d5dd9e68d32
shtheo <theo.ponton@gmail.com>
2019-02-20 17:57:35 +0100

    design

---
+++ onglets.js
@@ -0,0 +1,20 @@
+$(function() {
+	$('#onglets').css('display', 'block');
+	$('#onglets').click(function(event) {
+		var actuel = event.target;
+		if (!/li/i.test(actuel.nodeName) || actuel.className.indexOf('actif') > -1) {
+			alert(actuel.nodeName)
+			return;
+		}
+		$(actuel).addClass('actif').siblings().removeClass('actif');
+		setDisplay();
+	});
+	function setDisplay() {
+		var modeAffichage;
+		$('#onglets li').each(function(rang) {
+			modeAffichage = $(this).hasClass('actif') ? '' : 'none';
+			$('.item').eq(rang).css('display', modeAffichage);
+		});
+	}
+	setDisplay();
+});
